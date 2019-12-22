54050f05d10dd7d05e96a813e6a8ac4ec4acd4f0
prafullajoshi <joshiprafulla21@gmail.com>
2019-12-22 16:14:49 +0530

    Function programs added

---
+++ 04-Functions/01-EntryPointFunction/03-Type_03/EntryPointFunction.c
@@ -0,0 +1,12 @@
+#include<stdio.h> // stdio.h contains declaration printf()
+
+	// Entry Point Function => Valid Return Type and 1 Parameter (int argc)
+	
+int main(int argc)
+{
+	// code
+	printf("\n\n");
+	printf("Hello World !!\n");		// library function
+	printf("Number of command line arguments :%d\n\n",argc);
+	return 0;
+}
