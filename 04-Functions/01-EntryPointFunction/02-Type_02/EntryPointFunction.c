7db36272e262affbc6dafb42fca1dc4ba8f3d269
prafullajoshi <joshiprafulla21@gmail.com>
2019-12-22 16:14:27 +0530

    Function programs added

---
+++ 04-Functions/01-EntryPointFunction/02-Type_02/EntryPointFunction.c
@@ -0,0 +1,9 @@
+#include<stdio.h> // stdio.h contains declaration printf()
+
+	// Entry Point Function => Valid Return Type and No Parameters, hence no command line arguments
+
+int main(void)
+{
+	printf("Hello World !!\n");		// library function
+	return 0;
+}
