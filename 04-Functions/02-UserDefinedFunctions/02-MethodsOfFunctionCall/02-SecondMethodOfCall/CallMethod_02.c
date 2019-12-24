0aad5279e4b54f196795ae6d6a43525e3d4fc918
prafullajoshi <joshiprafulla21@gmail.com>
2019-12-24 23:45:32 +0530

    Function programs added

---
+++ 04-Functions/02-UserDefinedFunctions/02-MethodsOfFunctionCall/02-SecondMethodOfCall/CallMethod_02.c
@@ -0,0 +1,102 @@
+#include <stdio.h> //'stdio.h' contains declaration of 'printf()'
+
+// ****** USER DEFINED FUNCTIONS : METHOD OF CALLING FUNCTION 2 ******
+// **** CALLING ONLY TWO FUNCTIONS DIRECTLY IN main(), REST OF THE FUNCTIONS TRACE THEIR CALL INDIRECTLY TO main() ****/
+
+int main(int argc, char *argv[], char *envp[])
+{
+	//function prototypes
+	void display_information(void);
+	void Function_Country(void);
+
+	//code
+	display_information(); //function call
+	Function_Country(); //function call
+
+	return 0;
+}
+
+// ****** User-Defined Functions' Definitions... ***
+void display_information(void) //function definition
+{
+	//function prototypes
+	void Function_My(void);
+	void Function_Name(void);
+	void Function_Is(void);
+	void Function_FirstName(void);
+	void Function_MiddleName(void);
+	void Function_Surname(void);
+	void Function_OfAMC(void);
+
+	//code
+
+	// *** FUNCTION CALLS ***
+	Function_My();
+	Function_Name();
+	Function_Is();
+	Function_FirstName();
+	Function_MiddleName();
+	Function_Surname();
+	Function_OfAMC();
+}
+
+void Function_My(void) //function definition
+{
+	//code
+	printf("\n");
+
+	printf("My");
+}
+
+void Function_Name(void) //function definition
+{
+	//code
+	printf("\n");
+
+	printf("Name");
+}
+
+void Function_Is(void) //function definition
+{
+	//code
+	printf("\n");
+
+	printf("Is");
+}
+
+void Function_FirstName(void) //function definition
+{
+	printf("\n");
+
+	printf("Prafulla");
+}
+
+void Function_MiddleName(void) //function definition
+{
+	printf("\n");
+
+	printf("Suresh");
+}
+
+void Function_Surname(void) //function definition
+{
+	printf("\n");
+
+	printf("Joshi");
+}
+ 
+void Function_OfAMC(void) //function definition
+{
+	printf("\n");
+
+	printf("Of ASTROMEDICOMP");
+}
+
+void Function_Country(void) //function definition
+{
+	printf("\n");
+	
+	printf("I live In India.");
+
+	printf("\n\n");
+}
