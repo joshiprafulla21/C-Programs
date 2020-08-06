00f99dba3434e259217635d707c12ab24a39a925
Prafulla Joshi <joshiprafulla21@gmail.com>
2020-08-06 11:44:51 +0530

    Pattern Programs

---
+++ 08-Patterns/05-Pattern_05.c
@@ -0,0 +1,28 @@
+/*
+	Write a program to accept no of rows from user and print following pattern
+                      *
+                    * * *
+                  * * * * *
+                * * * * * * *
+              * * * * * * * * *
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    //Code
+    int i, j, k, numberOfRows;
+    printf("Enter the number of rows :");
+    scanf("%d", &numberOfRows);
+
+    for (i = 1; i <= numberOfRows; i++)
+    {
+        for (j = 1; j <= 2 * (numberOfRows - i); j++)
+            printf(" ");
+        for (k = 1; k <= (2 * i) - 1; k++)
+            printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
